20776e8611257fb5218cc3d3f9a13563aac8c449
Moud Mpofu <mpofunkazi@gmail.com>
2024-01-07 14:15:51 +0100

    added the AI api(shecodes) integration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 function generatePoem(event) {
   event.preventDefault();
+  let promptInput = document.querySelector("#prompt-input");
+  let key = "b93bfbo44bd8a88678e0t635d05036d5";
+  let context =
+    "You are an expert in writing cool poems, please generate a short Poem in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions below. ";
+  let prompt = `User instructions: Generate a poem about ${promptInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+  axios.get(apiUrl).then(displayPoem);
 
+  console.log("Generating poem..");
+}
+
+function displayPoem(response) {
+  console.log(response.data);
   new Typewriter("#poem", {
-    strings: "",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 1,
